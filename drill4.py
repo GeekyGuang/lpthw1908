2720e5b8d2db8ae007a1f151f563c754b3bfcfd9
Travis <travis.x.wong@gmail.com>
2019-08-21 15:55:56 +0800

    ss

---
+++ drill4.py
@@ -0,0 +1,4 @@
+aa = 2.0
+print(aa)
+bb = '你好！'
+print(aa , bb)
---
+++ ex5.py
@@ -0,0 +1,18 @@
+Name = 'Zed A. Shaw'
+Age = 35 # not a lie
+Height = 74 # inches
+Weight = 180 # lbs
+Eyes = 'Blue'
+Teeth = 'White'
+Hair = 'Brown'
+
+print(f"Let's talk about {Name}.")
+print(f"He's {Height} inches tall.")
+print(f"He's {Weight} pounds heavy.")
+print(f"Actually that's not too heavy.")
+print(f"He's got {Eyes} eyes and {Hair} hair.")
+print(f"His teeth are usually {Teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total = Age + Height + Weight 
+print(f"If I add {Age}, {Height}, and {Weight} I get {total}.")
