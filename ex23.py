1ac45ccecfa2a7d3a58050f14187aa629d005cdf
Travis <travis.x.wong@gmail.com>
2019-08-29 16:41:57 +0800

    ex23

--- ex23.py
+++ ex23.py
@@ -1,12 +1,14 @@
 import sys
 script, encoding, error = sys.argv
 
+
 def main(language_file, encoding, errors):
     line = language_file.readline()
 
     if line:
         print_line(line, encoding, errors)
         return main(language_file, encoding, errors)
+
 
 def print_line(line, encoding, errors):
     next_lang = line.strip()
